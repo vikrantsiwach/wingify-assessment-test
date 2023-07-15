8ae5864bdc85af2f94b6f12b13821fa5bf3dd455
Vikrant Siwach <vikrant.siwach@devslane.com>
2023-07-15 11:41:53 +0530

    project setup complete include tailwind

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import MainComponent from './components/MainComponent';
+import './index.css'
+
+function App() {
+
+  return (
+    <MainComponent  />
+  );
+}
+
+export default App
---
+++ src/components/MainComponent.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+interface Props {
+  
+}
+
+const MainComponent: React.FC<Props> = (props: Props) => {
+  return (
+    <div>Hello world</div>
+  );
+};
+
+export default MainComponent;
